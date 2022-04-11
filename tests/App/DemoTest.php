7729a8239323e99a13afbfa9305de0dc17b1a56f
jack <jack@ks-it.co>
2022-04-11 17:03:34 +0800

    add Empty DemoTest

---
+++ tests/App/DemoTest.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * @Date         : 2022-03-02 14:49:25
+ * @LastEditors  : Jack Zhou <jack@ks-it.co>
+ * @LastEditTime : 2022-03-02 17:22:16
+ * @Description  : 
+ * @FilePath     : /recruitment-php-code-test/tests/App/DemoTest.php
+ */
+
+namespace Test\App;
+
+use PHPUnit\Framework\TestCase;
+
+
+class DemoTest extends TestCase {
+
+    public function test_foo() {
+
+    }
+
+    public function test_get_user_info() {
+
+    }
+}
